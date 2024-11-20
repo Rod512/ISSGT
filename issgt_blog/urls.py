dd52a20f64bfae592254bb87989ad10b8aaa8ff9
Rod512 <rudro149187@bsdi-bd.org>
2024-11-21 02:10:40 +0530

    login & registration from

--- issgt_blog/urls.py
+++ issgt_blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('blogs/', include('blogs.urls')),
+    path('account/', include('account.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
