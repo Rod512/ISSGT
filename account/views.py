0def0d091d1b2099373af759b2fcd27e09bc8b9a
Rod512 <rudro149187@bsdi-bd.org>
2024-11-25 00:27:30 +0530

    login credentials bug

--- account/views.py
+++ account/views.py
@@ -1,18 +1,10 @@
-from django.shortcuts import render,redirect
-from .forms import RegistrationForm
-from django.contrib import messages
-from .models import Account
-
-def login(request): 
-    return render(request, 'account/login.html')
-
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from django.contrib import messages
 from .models import Account
+from django.contrib.auth import authenticate, login
+from urllib.parse import urlparse, parse_qs
 
-def login(request): 
-    return render(request, 'account/login.html')
 
 def registration(request):
     if request.method == 'POST':
@@ -30,7 +22,7 @@ def registration(request):
             
             # Save phone number
             user.phone_number = phone_number
-            user.save()  # Save the user object with the phone number
+            user.save()  
             
             # Success message
             messages.success(request, 'Your account has been created! You can now log in.')
@@ -39,4 +31,35 @@ def registration(request):
         form = RegistrationForm()
     
     return render(request, 'account/registration.html', {'form': form})
+
+
+
+
+def user_login(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        # Authenticate using the default authentication system
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            # Log the user in if authentication is successful
+            login(request, user)
+            messages.info(request, "You have successfully logged in.")
+            url = request.META.get("HTTP_REFERER")
+            try:
+                query = urlparse(url).query
+                params = dict(x.split("=") for x in query.split("&"))
+                if "next" in params:
+                    nextPage = params["next"]
+                    return redirect(nextPage)
+            except Exception:
+                return redirect("dashboard")  # Replace with your dashboard or homepage URL
+        else:
+            messages.warning(request, "Invalid login credentials")
+            return redirect("login")
+    else:
+        return render(request, "account/login.html")
+
 
