5329e078ae0dac2f95ebefaaed60efb61421bfa2
Rod512 <rudro149187@bsdi-bd.org>
2025-01-06 22:29:20 +0530

    search option updated

--- home/views.py
+++ home/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 
-from .models import HomeBlog
+from .models import HomeBlog, Category
 from django.utils.timezone import now
 from django.db.models import Q
-# from .forms import SearchForm
 from django.contrib import messages
 
 
@@ -11,14 +10,21 @@ def home(request):
     featured_blogs = HomeBlog.objects.filter(is_featured=True).order_by('-published_date')
     if not featured_blogs.exists():
         featured_blogs = HomeBlog.objects.all().order_by('-published_date')  
-
-    # টেমপ্লেটের জন্য `blogs` কনটেক্সট যোগ করো
     blogs = HomeBlog.objects.all().order_by('-published_date')
+    categories = Category.objects.all()
+
+    blogs_by_category = {
+        category.name: HomeBlog.objects.filter(category=category).order_by('-published_date')[:5]
+        for category in categories if HomeBlog.objects.filter(category=category).exists()
+    }
+
+
 
     context = {
         'featured_blogs': featured_blogs,
-        'blogs': blogs,
+        'blogs_by_category': blogs_by_category,
         'version': now().timestamp(),
+        'blogs' : blogs
     }
     return render(request, 'home/index.html', context)
 
@@ -35,15 +41,32 @@ def search(request):
             Q(content__icontains=keyword) |
             Q(author__username__icontains=keyword)
         ).order_by('-published_date')
+        messages.info(request, f"Search results for '{keyword}': {blogs.count()} blogs found.")
     else:
-        blogs = HomeBlog.objects.all().order_by('-published_date')
-        print(f"Found {blogs.count()} blogs")
+        blogs = HomeBlog.objects.none()  # No keyword, no blogs
+
+    featured_blogs = HomeBlog.objects.filter(is_featured=True).order_by('-published_date')
+    if not featured_blogs.exists():
+        featured_blogs = HomeBlog.objects.all().order_by('-published_date')
+
+    categories = Category.objects.all()
+    blogs_by_category = {
+        category.name: HomeBlog.objects.filter(category=category).order_by('-published_date')[:5]
+        for category in categories if HomeBlog.objects.filter(category=category).exists()
+    }
 
     context = {
-        'blogs': blogs,
+        'featured_blogs': featured_blogs,
+        'blogs_by_category': blogs_by_category,
+        'blogs': blogs,  
+        'search_keyword': keyword,
+        'version': now().timestamp(),
     }
 
     return render(request, 'home/index.html', context)
+
+
+
 
 
 
